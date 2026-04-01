6ad82b2024fd287ea1ca0ff67b8798413d840968
meghanakj2005 <meghanaprakash2005@gmail.com>
2026-04-01 10:00:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:5000";
+const BASE_URL = "";
 
 const listContainer = document.getElementById("listContainer");
 const details = document.getElementById("details");
